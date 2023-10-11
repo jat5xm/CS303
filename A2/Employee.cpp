c72b2fa81428d4163b828f6cff7b979c8e881972
jat5xm <113187220+jat5xm@users.noreply.github.com>
2023-10-10 23:28:35 -0500

    Create Employee.cpp

---
+++ A2/Employee.cpp
@@ -0,0 +1,5 @@
+#include "Employee.h"
+
+
+
+Employee::Employee(string name, double hourlyRate) : name(name), hourlyRate(hourlyRate) {} // The pure virtual functions aren't implemented here because they'll be implemented in the derived classes
